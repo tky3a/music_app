6dc4311b0f7b2716198da886c010e0efae16a6ba
tky3a <takayamac@icloud.com>
2018-04-22 19:45:48 +0900

    users view edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
   #保存する前にemailを小文字に変更する
   before_save {self.email = email.downcase } #selfは現在のユーザーを指す
-  #nameのパリデーション: 空では登録不可,長さは50文字以内
+  #nameのパリデーション: 空では登録不可,長さは最大50文字
   validates :name, presence: true, length: { maximum: 50 }
   #emailのバリデーション: 正規表現と一致するメールアドレスと255文字以内のメールアドレスが登録可
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #正規表現を代入
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with:VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false } #重複した場合false
-
+  #セキュアなパスワードを指定
   has_secure_password
+  #passwordのバリデーション
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
