7c2076d143ceb5f438c559f47a7e5e3dd84738f7
tky3a <takayamac@icloud.com>
2018-04-23 23:57:04 +0900

    iframe thumbnails

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,14 +1,14 @@
 module VideosHelper
-  def video(opts)
-    iframe = content_tag(
-      :iframe,
-      '', # empty body
-      width: 250,
-      height: 200,
-      src: "https://www.youtube.com/embed/#{opts[:video_id]}",
-      frameborder: 4,
-      allowfullscreen: true
-    )
-    content_tag(:div, iframe, class: 'youtube-container')
-  end
+  # def video(opts)
+  #   iframe = content_tag(
+  #     :iframe,
+  #     '', # empty body
+  #     width: 250,
+  #     height: 200,
+  #     src: "https://www.youtube.com/embed/#{opts[:video_id]}",
+  #     frameborder: 4,
+  #     allowfullscreen: true
+  #   )
+  #   content_tag(:div, iframe, class: 'youtube-container')
+  # end
 end
