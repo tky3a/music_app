dffbf8b596b80bcc7902cc1cb2152e7212ee9eee
tky3a <takayamac@icloud.com>
2018-06-08 18:58:45 +0900

    create seeds

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -15,14 +15,14 @@ class VideosController < ApplicationController
   def create
     @video = Video.new(video_params)
       if @video.save
-        @category = Category.find_by(name: params[:category][:name])#categoryのnameを取得
-        @video_cate = VideoCategory.new(video_id: @video.id, category_id: @category.id)#video_idとcategory_id取得
-        if @video_cate.save
-          redirect_to @video
+        # @category = Category.find_by(name: params[:category][:name])#categoryのnameを取得
+        # @video_cate = VideoCategory.new(video_id: @video.id, category_id: @category.id)#video_idとcategory_id取得
+        # if @video_cate.save
+        #   redirect_to @video
         else
           render 'new'
         end
-      end
+      # end
   end
 
 #page link
@@ -50,6 +50,6 @@ class VideosController < ApplicationController
  #strongparams
   private
     def video_params
-        params.require(:video).permit(:artist_name, :youtube_id, :music_name, { :category_id => [] })
+        params.require(:video).permit(:artist_name, :youtube_id, :music_name, { category_ids: [] })
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 5.times do |i|
+#   users = User.create(username: "test User#{i}",
+#                       email: "testuser#{i}@example.com",
+#                       password: "hogehoge",
+#                       password_confirmation: "hogehoge")
+# end
+
+  video_test1 = Video.create(youtube_id: "m2bzXSKous4",
+                        artist_name: "アルカラ",
+                        music_name: "サースティサースティサースティガール")
+
+  video_test2 = Video.create(youtube_id: "WJW7kS9AAI4",
+                        artist_name: "ポルカドットスティングレイ",
+                        music_name: "ICHIDAIJI")
+
+  category_test1 = Category.create(name: "Rock")
+  category_test2 = Category.create(name: "Gt Rock")
+  category_test3 = Category.create(name: "Emo")
+  category_test4 = Category.create(name: "Post Rock")
+  # category_test1 = video_test1.categories.create(name: "Rock")
+  # category_test2 = video_test1.categories.create(name: "Gt_Rock")
