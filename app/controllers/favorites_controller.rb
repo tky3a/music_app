c9680af80a3518c1f0c8890d58cca92d2ad5a8f7
tky3a <takayamac@icloud.com>
2018-06-16 16:50:12 +0900

    comment custom

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  def create
+    @user_id = current_user.id
+    @video_id = Video.find(params[:id]).id
+    @favorite = Favorite.new(user_id: @user_id, video_id: @video_id)
+
+    @favorite.save
+  end
+
+  def destroy
+
+  end
+end
