bb08ffdb1453ae539908e56525ff11154f070e6a
tky3a <takayamac@icloud.com>
2018-06-30 19:54:49 +0900

    Add a User or Video save testcode

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user save" do
+    user = User.new({
+      username: "hogehoge",
+      email: "hogehoge@example.com",
+      password: "password12345",
+      password_confirmation: "password12345"
+      })
+      assert user.save, "true"
+  end
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class VideoTest < ActiveSupport::TestCase
-  # test "should be valid" do
-  #   assert @video.valid?
-  # end
+  test "video save test" do
+    video = Video.new({
+      youtube_id: "w_3atGoniPM",
+      artist_name: "yonige",
+      music_name: "リボルバー"
+      })
+      assert video.save, 'Failed to save'
+  end
 end
